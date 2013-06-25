05402476a456dd6cd2254b3d000dc09d0b878f68
Alan Briolat <alan.briolat@gmail.com>
2013-06-25 23:47:59 +0100

    Add all instructions to "ops" array

--- src/hexico/foostack/Asm.scala
+++ src/hexico/foostack/Asm.scala
@@ -20,41 +20,37 @@ object Asm {
 
   val ops = Map(
     "LDBI" -> OpHI,
+    "LDI" -> OpRI,
+    "LD" -> OpRR,
+    "LDB" -> OpHH,
+    "PUSH" -> OpR,
+    "POP" -> OpR,
+    "PEEK" -> OpR,
+    "ADD" -> OpRR,
+    "ADDI" -> OpRI,
+    "ADDC" -> OpRR,
+    "SUB" -> OpRR,
+    "SUBI" -> OpRI,
+    "SUBB" -> OpRR,
+    "NEG" -> OpR,
+    "AND" -> OpRR,
+    "OR" -> OpRR,
+    "XOR" -> OpRR,
+    "NOT" -> OpR,
+    "SHL" -> OpRR,
+    "SHLI" -> OpRI,
+    "SHR" -> OpRR,
+    "SHRI" -> OpRI,
+    "SSL" -> OpRR,
+    "SSLI" -> OpRI,
+    "SSR" -> OpRR,
+    "SSRI" -> OpRI,
+    "JMP" -> OpR,
+    "JR" -> OpR,
+    "JRI" -> OpI,
+    "JMPNZ" -> OpR,
+    "JRNZ" -> OpR,
+    "JRINZ" -> OpI,
     "HALT" -> Op_
   )
-
-  sealed abstract class Instruction
-  case class HALT() extends Instruction
-  case class LDBI(r: HalfRegister.Value, b: Byte) extends Instruction
-  case class LDI(r: Register.Value, b: Byte) extends Instruction
-  case class LD(r1: Register.Value , r2: Register.Value) extends Instruction
-  case class LDB(r1: HalfRegister.Value, r2: HalfRegister.Value) extends Instruction
-  case class PUSH(r: Register.Value) extends Instruction
-  case class POP(r: Register.Value) extends Instruction
-  case class PEEK(r: Register.Value) extends Instruction
-  case class ADD(r1: Register.Value, r2: Register.Value) extends Instruction
-  case class ADDI(r: Register.Value, b: Byte) extends Instruction
-  case class ADDC(r1: Register.Value, r2: Register.Value) extends Instruction
-  case class SUB(r1: Register.Value, r2: Register.Value) extends Instruction
-  case class SUBI(r: Register.Value, b: Byte) extends Instruction
-  case class SUBB(r1: Register.Value, r2: Register.Value) extends Instruction
-  case class NEG(r: Register.Value) extends Instruction
-  case class AND(r1: Register.Value, r2: Register.Value) extends Instruction
-  case class OR(r1: Register.Value, r2: Register.Value) extends Instruction
-  case class XOR(r1: Register.Value, r2: Register.Value) extends Instruction
-  case class NOT(r: Register.Value) extends Instruction
-  case class SHL(r1: Register.Value, r2: Register.Value) extends Instruction
-  case class SHLI(r: Register.Value, b: Byte) extends Instruction
-  case class SHR(r1: Register.Value, r2: Register.Value) extends Instruction
-  case class SHRI(r: Register.Value, b: Byte) extends Instruction
-  case class SSL(r1: Register.Value, r2: Register.Value) extends Instruction
-  case class SSLI(r: Register.Value, b: Byte) extends Instruction
-  case class SSR(r1: Register.Value, r2: Register.Value) extends Instruction
-  case class SSRI(r: Register.Value, b: Byte) extends Instruction
-  case class JMP(r: Register.Value) extends Instruction
-  case class JR(r: Register.Value) extends Instruction
-  case class JRI(b: Byte) extends Instruction
-  case class JMPNZ(r: Register.Value) extends Instruction
-  case class JRNZ(r: Register.Value) extends Instruction
-  case class JRINZ(b: Byte) extends Instruction
 }
